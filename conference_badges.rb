ab25ce5d988cf25a01aca6d9d6c1c08943f5ff03
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-24 16:18:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  badge_list = []
+  attendees.each do |name|
+    badge_list.push("Hello, my name is #{name}.")
+  end
+  return badge_list
+end
